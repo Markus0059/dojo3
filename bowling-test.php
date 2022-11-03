43771e85c34ee6726fa04bc4aafc8a669000798c
Germe Marc Antoine <markus0059@hotmail.fr>
2022-11-03 15:55:49 +0100

    bowling

--- bowling-test.php
+++ bowling-test.php
@@ -1,21 +1,24 @@
-<?php 
+<?php
 
-use APP\Bowling;
+require 'vendor/autoload.php';
 
-function bowling() 
+use App\Bowling;
+
+function bowling()
 {
     $game = new Bowling();
-    $gameScores = [2, 4];
+    $gameScores = [2, 8, 10, 5];
     foreach ($gameScores as $score) {
         $game->newThrow($score);
     }
     try{
-        if($game->getCurrentScore() === 6){
+        if($game->getCurrentScore() === 16){
             echo "success ! your score is : " . $game->getCurrentScore();
         }
     }catch(Exception $error){
         echo $error->getMessage();
     }
+
 };
 
 bowling();
--- src/Bowling.php
+++ src/Bowling.php
@@ -1,12 +1,45 @@
 <?php
+namespace App;
 
 class Bowling {
-    
-    public function newThrow ($pinsNbr)
+
+  private int $currentScore = 0;
+  private int $currentTurn = 1;
+  private int $currentThrow = 1;
+  private int $pinsLeft = 10;
+  private int $pinsNbr;
+
+    public function newThrow ($pinsNbr):void
     {
+      $this->pinsLeft = max(0, $this->pinsLeft - $pinsNbr);
+
+      if ($this->isSpare() || $this->isStrike() || $this->currentThrow >= 2){
+        $this->newTurn();
+      } else {
+        $this->currentThrow++;
+      }
+
+      $this->currentScore += $pinsNbr;
     }
 
-    public function getCurrentScore ()
-    {
+    public function isStrike(): bool{
+      return $this->pinsLeft === 0 && $this->currentThrow === 1;
     }
-}
+
+    public function isSpare(): bool{
+      return $this->pinsLeft === 0 && $this->currentThrow === 2;
+    }
+
+    public function getCurrentScore ():int
+    {
+      return $this->currentScore;
+    }
+
+    public function newTurn():void
+    {
+      $this->pinsLeft = 10;
+      $this->currentThrow = 1;
+      $this->currentTurn++;
+    }
+
+  }
